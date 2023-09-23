21bd34010f70db0fc26ebeb3269ce8ae35ddc7e1
Tosha177777 <slavkoanton4@gmail.com>
2023-09-23 15:52:18 +0300

    css

--- src/components/Form/Contacts.jsx
+++ src/components/Form/Contacts.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Formik, Form, Field } from 'formik';
 import { nanoid } from 'nanoid';
-import css from './Contacts.module.css';
 
 export default class Contacts extends Component {
   state = {
